00bc69bd75e562f390e347922e2b397fca684499
Abdullah Waqet <abdullah.waqet@hotmail.com>
2019-08-03 02:03:46 +0300

    update contracts

---
+++ project-6/contracts/coffeeaccesscontrol/PharmacyRole.sol.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.24;
+
+// Import the library 'Roles'
+import "./Roles.sol";
+
+// Define a contract 'PharmacyRole' to manage this role - add, remove, check
+contract PharmacyRole {
+  using Roles for Roles.Role;
+
+  // Define 2 events, one for Adding, and other for Removing
+  event PharmacyAdded(address indexed account);
+  event PharmacyRemoved(address indexed account);
+
+  // Define a struct 'pharmaces' by inheriting from 'Roles' library, struct Role
+  Roles.Role private pharmaces;
+
+  // In the constructor make the address that deploys this contract the 1st Pharmacy
+  constructor() public {
+    _addPharmacy(msg.sender);
+  }
+
+  // Define a modifier that checks to see if msg.sender has the appropriate role
+  modifier onlyPharmacy() {
+    require(isPharmacy(msg.sender), 'only Pharmacy can Access!');
+    _;
+  }
+
+  // Define a function 'isPharmacy' to check this role
+  function isPharmacy(address account) public view returns (bool) {
+    return pharmaces.has(account);
+  }
+
+  // Define a function 'addPharmacy' that adds this role
+  function addPharmacy(address account) public onlyPharmacy {
+    _addPharmacy(account);
+  }
+
+  // Define a function 'renouncePharmacy' to renounce this role
+  function renouncePharmacy() public {
+    _removePharmacy(msg.sender);
+  }
+
+  // Define an internal function '_addPharmacy' to add this role, called by 'addPharmacy'
+  function _addPharmacy(address account) internal {
+    pharmaces.add(account);
+    emit PharmacyAdded(account);
+  }
+
+  // Define an internal function '_removePharmacy' to remove this role, called by 'removePharmacy'
+  function _removePharmacy(address account) internal {
+    pharmaces.remove(account);
+    emit PharmacyRemoved(account);
+  }
+}
